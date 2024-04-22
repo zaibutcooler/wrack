99ca564f014ceca59de82adff7922af715f3c678
zaibutcooler <zaiyellyintaung@gmail.com>
2024-04-22 17:55:18 +0630

    huggingface and test cases

---
+++ tests/test_data.py
@@ -0,0 +1,12 @@
+# test_demo.py
+import unittest
+from wrack.data_prep import load_data
+
+class TestDataset(unittest.TestCase):
+    def test_download(self):
+        result = load_data()
+        assert len(result) > 0
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ wrack/data_prep.py
@@ -0,0 +1,6 @@
+
+
+def load_data(size):
+    url =  ''
+    
+    return [1]
